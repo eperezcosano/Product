23b914d0509499b681a531f6fa6176a77369041b
Izan Pérez Cosano <izanperez98@gmail.com>
2019-04-10 14:50:18 +0200

    first ProductManager

---
+++ src/main/java/edu/upc/dsa/ProductManager.java
@@ -0,0 +1,44 @@
+package edu.upc.dsa;
+
+import edu.upc.dsa.models.*;
+import edu.upc.dsa.exceptions.*;
+
+import java.util.List;
+
+public interface ProductManager {
+
+    /**
+     * List of ordered products (ascending) by price
+     * @return list of products
+     */
+    List<Product> getProductsByPrice();
+
+    /**
+     * Place an order (which can consist of different products and in
+     * different quantities) by an identified user
+     * @param products list of products
+     * @param idUser user identifier
+     * @throws UserNotFoundException if user does not exist
+     * @return order identifier
+     */
+    String placeOrder(List<Product> products, String idUser) throws UserNotFoundException;
+
+    /**
+     * Serve an order. The services are carried out in order of arrivals
+     */
+    void serveOrder(Order order);
+
+    /**
+     * List of orders of a user that have already been made
+     * @param idUser user identifier
+     * @throws UserNotFoundException if user does not exist
+     * @return list of orders
+     */
+    List<Order> getPlacedOrders(String idUser) throws UserNotFoundException;
+
+    /**
+     * List of ordered products (descending) by number of sales
+     * @return list of products
+     */
+    List<Product> getProductsBySales();
+}
