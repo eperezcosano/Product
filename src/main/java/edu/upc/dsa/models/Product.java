8d2eb751d2390b4eaf58ff4260362ab66c55c233
Izan Pérez Cosano <izanperez98@gmail.com>
2019-04-10 14:55:07 +0200

    Product

--- src/main/java/edu/upc/dsa/models/Product.java
+++ src/main/java/edu/upc/dsa/models/Product.java
@@ -1,4 +1,46 @@
 package edu.upc.dsa.models;
 
 public class Product {
+
+    //Attributes
+    private String idProduct;
+    private String name;
+    private double price;
+
+    //Constructors
+    public Product() {
+
+    }
+
+    public Product(String idProduct, String name, double price) {
+        this.idProduct = idProduct;
+        this.name = name;
+        this.price = price;
+    }
+
+    //Getters and setters
+    public String getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(String idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
