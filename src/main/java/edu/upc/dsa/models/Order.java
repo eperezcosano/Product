23a925fb5c991d2f14090344391f6fb2db5d2654
Izan Pérez Cosano <izanperez98@gmail.com>
2019-04-10 14:59:47 +0200

    Order

--- src/main/java/edu/upc/dsa/models/Order.java
+++ src/main/java/edu/upc/dsa/models/Order.java
@@ -1,4 +1,39 @@
 package edu.upc.dsa.models;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Order {
+
+    //Attributes
+    private String idOrder;
+    private List<Product> products;
+
+    //Constructors
+    public Order() {
+        this.products = new LinkedList<>();
+    }
+
+    public Order(String idOrder) {
+        this.idOrder = idOrder;
+        this.products = new LinkedList<>();
+    }
+
+    //Getters and setters
+    public String getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(String idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
 }
