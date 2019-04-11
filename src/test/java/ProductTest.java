acc0187c37bd0cd0a9d467c573c7b8d49de37f28
Izan Pérez Cosano <izanperez98@gmail.com>
2019-04-11 10:45:59 +0200

    test finnished

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -2,7 +2,6 @@ import edu.upc.dsa.ProductManager;
 import edu.upc.dsa.ProductManagerImpl;
 import edu.upc.dsa.exceptions.ProductNotFoundException;
 import edu.upc.dsa.exceptions.UserNotFoundException;
-import edu.upc.dsa.models.Order;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -64,8 +63,59 @@ public class ProductTest {
         Assert.assertEquals(1, this.productManager.numOrders());
     }
 
-    @Test
-    public void testServeOrder() {
+    @Test(expected = ProductNotFoundException.class)
+    public void testPlaceOrderProductNotFound() throws ProductNotFoundException, UserNotFoundException {
 
+        List<String> list = new ArrayList<>();
+        list.add("Strawberry");
+        list.add("Apple");
+        list.add("Pizza");
+
+        Assert.assertEquals(0, this.productManager.numOrders());
+
+        this.productManager.placeOrder("My Shopping List", list, "User1");
+
+        Assert.assertEquals(0, this.productManager.numOrders());
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    public void testPlaceOrderUserNotFound() throws ProductNotFoundException, UserNotFoundException {
+
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Apple");
+        list.add("Pizza");
+
+        Assert.assertEquals(0, this.productManager.numOrders());
+
+        this.productManager.placeOrder("My Shopping List", list, "UserX");
+
+        Assert.assertEquals(0, this.productManager.numOrders());
+    }
+
+    @Test
+    public void testServeOrder() throws ProductNotFoundException, UserNotFoundException {
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("Apple");
+        list1.add("Apple");
+        list1.add("Pizza");
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("Pizza");
+        list2.add("Hamburger");
+        list2.add("Apple");
+
+        Assert.assertEquals(0, this.productManager.numOrders());
+
+        this.productManager.placeOrder("My Shopping List 0", list1, "User1");
+        this.productManager.placeOrder("My Shopping List 1", list2, "User2");
+        this.productManager.placeOrder("My Shopping List 2", list2, "User1");
+
+        Assert.assertEquals(3, this.productManager.numOrders());
+
+        this.productManager.serveOrder();
+
+        Assert.assertEquals(2, this.productManager.numOrders());
     }
 }
